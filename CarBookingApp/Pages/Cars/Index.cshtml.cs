6d3fd126b9951a75b5e0730772435df315fda2ee
saba satseradze <saba.satseradze0@gmail.com>
2023-07-01 08:12:11 -0700

    init: CarBookDbContext

---
+++ CarBookingApp/Pages/Cars/Index.cshtml.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CarBookingApp.Pages.Cars
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IConfiguration _configuration;
+
+        public IndexModel(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
+
+        public string Heading { get; set; }
+
+        public string Message { get; set; }
+        public void OnGet()
+        {
+            Heading = "List of Cars";
+            Message = _configuration["Message"];
+        }
+    }
+}
